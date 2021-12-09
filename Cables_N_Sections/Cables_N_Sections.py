b2d9b7f8b1eb70336efa2d095c11f8522c761e3b
predybalov <predybalov@gmail.com>
2021-12-09 16:23:15 +0300

    Clean up and minor refactoring

--- Cables_N_Sections/Cables_N_Sections.py
+++ Cables_N_Sections/Cables_N_Sections.py
@@ -8,7 +8,6 @@
 """
 
 import win32com.client as wc
-import collections
 import copy
 import time
 
@@ -17,9 +16,7 @@ xl_app = wc.Dispatch("Excel.Application").Sheets("Work")
 
 start = time.time()
 
-data_set = {}
-section_set = []
-cable_set = []
+section_set, cable_set_unsort = [], []
 cabline_1, cabline_2 = '', ''
 
 # Iterate trough all objects (entities) in the currently opened drawing
@@ -34,203 +31,191 @@ for block in acad_app:
             elif attribute.TagString == 'CABLES2':
                 if len(attribute.TextString) > 0:
                     cabline_2 = cabline_1 + ',' + attribute.TextString
-                    cable_set.append(cabline_2.split(','))
+                    cable_set_unsort.append(cabline_2.split(','))
                 else:
                     cabline_2 = cabline_1
-                    cable_set.append(cabline_2.split(','))
+                    cable_set_unsort.append(cabline_2.split(','))
 
 
+# Convert cables to float and sort
+cable_set = []
 
-
-
-
-
-
-xxx = []
-for value in cable_set:
-    zzz = []
-    for c in value:
+for s in cable_set_unsort:
+    temp_set = []
+    for c in s:
         if c[-1].lower() == 'a' or c[-1].lower() == 'а':
-            zzz.append(float(c[:-1] + '.1'))
+            temp_set.append(float(c[:-1] + '.1'))
         else:
-            zzz.append(float(c))
-        zzz.sort()
-    xxx.append(zzz)
+            temp_set.append(float(c))
+        temp_set.sort()
+    cable_set.append(temp_set)
 
 
-
-sum = []
+# Create sorted dictionary {section: [cable numbers]}
+common_set_temp = []
+common_set_unsort, common_set = {}, {}
 
 for i in range(len(section_set)):
-    sum.append(section_set[i])
-    sum.append(xxx[i])
+    common_set_temp.append(section_set[i])
+    common_set_temp.append(cable_set[i])
 
-for i in range(0, len(sum), 2):
-    data_set[sum[i]] = sum[i + 1]
+for i in range(0, len(common_set_temp), 2):
+    common_set_unsort[common_set_temp[i]] = common_set_temp[i + 1]
+
+cs = list(common_set_unsort.keys())
+cs.sort()
+
+for c in cs:
+    common_set[c] = common_set_unsort[c]
 
 
-data_set = collections.OrderedDict(sorted(data_set.items()))
+# Create two copies of the common set for further processing
+common_set_2 = copy.deepcopy(common_set)
+common_set_3 = copy.deepcopy(common_set)
 
 
-###
-data2 = copy.deepcopy(data_set)
-###
-data3 = copy.deepcopy(data_set)
-###
-#
-#
-# # Set text format for cells
-# xl_app.Range("R:Z").NumberFormat = "@"
-#
-#
-# # Iterate through key:value pairs in the dictionary and fill sections
-# # Add header
-# xl_app.Range("Y1").Value, xl_app.Range("Z1").Value = 'Номер сечения', 'Номер кабеля'
-#
-# line = ''
-# length = 50
-# counter = 2
-# column = 25
-# i = 1
-#
-# for key in section_set:
-#     xl_app.Cells(counter, column).Value = 'A' + str(key)
-#     while len(section_set[key]) > 0:
-#         if (len(line) + len(str(section_set[key][0]))) < length:
-#             if str(section_set[key][0])[-1] == '0':
-#                 line = line + '3-' + str(section_set[key][0])[:-2] + ', '
-#                 section_set[key].pop(0)
-#             else:
-#                 line = line + '3-' + str(section_set[key][0])[:-2] + 'а' + ', '
-#                 section_set[key].pop(0)
-#         else:
-#             xl_app.Cells(counter, column+1).Value = line[:-1]
-#             line = ''
-#             counter += 1
-#
-#     xl_app.Cells(counter, column+1).Value = line[:-2]
-#     line = ''
-#     counter += 1
-#     i += 1
-#
-#
-# # Create list of the cables
-# numbers = []
-# for key in data2:
-#     numbers += data2[key]
-# numbers_set = sorted(set(numbers))
-#
-#
-# final_numbers = []
-# for cable in numbers_set:
-#     if cable != int(cable):
-#         final_numbers.append(str(cable))
-#     else:
-#         final_numbers.append(str(cable))
-#
-#
-# data10 = {}
-#
-# for number in final_numbers:
-#     tmpf = []
-#     for value in data3:
-#         if float(number) in data3[value]:
-#             tmpf.append(value)
-#
-#     data10[float(number)] = tmpf
-#
-# data11 = collections.OrderedDict(sorted(data10.items()))
-#
-# # Iterate through key:value pairs in the dictionary and fill cables
-# line = ''
-# counter = 1
-# length = 45
-# column = 18
-#
-# for key in data11:
-#     if str(key)[-2:] == '.1':
-#         xl_app.Cells(counter, column).Value = '3-' + str(key)[:-2] + 'а'
-#     elif str(key)[-2:] == '.0':
-#         xl_app.Cells(counter, column).Value = '3-' + str(key)[:-2]
-#     else:
-#         print('Ошибка')
-#
-#     mount = 0
-#
-#     while len(data11[key]) > 0:
-#         if (len(line) + len(str(data11[key][0]))) < length:
-#             if str(data11[key][0])[-1] == '0':
-#                 line = line + 'А' + str(data11[key][0]) + ', '
-#                 data11[key].pop(0)
-#             else:
-#                 line = line + 'А' + str(data11[key][0]) + ', '
-#                 data11[key].pop(0)
-#         else:
-#             xl_app.Cells(counter, column + 1).Value = line[:-1]
-#             line = ''
-#             counter += 1
-#             mount += 1
-#
-#     xl_app.Cells(counter, column + 1).Value = line[:-2]
-#     line = ''
-#     counter += 1
-#     i += 1
-#     if mount == 0:
-#         xl_app.Cells(counter, column + 1).Value = ''
-#         counter += 1
-#
-# end_line = 2000
-#
-# # Fill cable journal
-# for i in range(1, end_line):
-#     if xl_app.Cells(i, 1).Value == '' or xl_app.Cells(i, 1).Value == xl_app.Cells(i, 18).Value:
-#         continue
-#     else:
-#         r = 'A' + str(i) + ':' + 'P' + str(i)
-#         xl_app.Range(r).Insert()
-#
-# for i in range(1, end_line):
-#     xl_app.Cells(i, 12).Value = xl_app.Cells(i, 19).Value
-#
-# xl_app.Columns(18).Clear()
-# xl_app.Columns(19).Clear()
-#
-#
-# # Add blank lines to fit document format (first page - 24 lines, subsequent pages - 30 lines)
-# prev = 0
-# page = 1
-# lines1 = 24
-#
-# for current in range(1, lines1 + 10):
-#     if xl_app.Cells(current, 1).Value != '' and xl_app.Cells(current, 1).Value is not None:
-#         if prev <= lines1*page and current > lines1*page + 1:
-#             j = prev
-#             step = lines1*page - prev + 1
-#             for j in range(step):
-#                 r = 'A' + str(prev) + ':' + 'P' + str(prev)
-#                 xl_app.Range(r).Insert()
-#             page += 1
-#         elif current > lines1*page:
-#             page += 1
-#         prev = current
-#
-#
-# lines2 = 30
-# page = 1
-#
-# for current in range(25, end_line):
-#     if xl_app.Cells(current, 1).Value != '' and xl_app.Cells(current, 1).Value is not None:
-#         if prev <= lines2*page + lines1 and current > lines2*page + lines1 + 1:
-#             j = prev
-#             step = lines2*page + lines1 - prev + 1
-#             for j in range(step):
-#                 r = 'A' + str(prev) + ':' + 'P' + str(prev)
-#                 xl_app.Range(r).Insert()
-#             page += 1
-#         elif current > lines2*page + lines1:
-#             page += 1
-#         prev = current
-#
-#
-# stop = time.time()
-# print(round(stop - start), 'sec')
+# Set text format for cells
+xl_app.Range("R:Z").NumberFormat = "@"
+
+
+# Iterate through key:value pairs in the dictionary and fill sections
+
+# Add header
+xl_app.Range("Y1").Value, xl_app.Range("Z1").Value = 'Номер сечения', 'Номер кабеля'
+
+line = ''
+length = 50
+counter = 2
+column = 25
+i = 1
+
+for s in common_set:
+    xl_app.Cells(counter, column).Value = 'A' + str(s)
+    while len(common_set[s]) > 0:
+        if (len(line) + len(str(common_set[s][0]))) < length:
+            if str(common_set[s][0])[-1] == '0':
+                line = line + '3-' + str(common_set[s][0])[:-2] + ', '
+                common_set[s].pop(0)
+            else:
+                line = line + '3-' + str(common_set[s][0])[:-2] + 'а' + ', '
+                common_set[s].pop(0)
+        else:
+            xl_app.Cells(counter, column+1).Value = line[:-1]
+            line = ''
+            counter += 1
+
+    xl_app.Cells(counter, column+1).Value = line[:-2]
+    line = ''
+    counter += 1
+    i += 1
+
+
+# Create Create sorted dictionary {cable: [section numbers]}
+numbers = []
+cabsec = {}
+
+for c in common_set_2:
+    numbers += common_set_2[c]
+numbers_set = sorted(set(numbers))
+
+for c in numbers_set:
+    tmp = []
+    for value in common_set_3:
+        if c in common_set_3[value]:
+            tmp.append(value)
+    cabsec[c] = tmp
+
+
+# Iterate through key:value pairs in the dictionary and fill cables
+line = ''
+counter = 1
+length = 45
+column = 18
+
+for c in cabsec:
+    if str(c)[-2:] == '.1':
+        xl_app.Cells(counter, column).Value = '3-' + str(c)[:-2] + 'а'
+    elif str(c)[-2:] == '.0':
+        xl_app.Cells(counter, column).Value = '3-' + str(c)[:-2]
+    else:
+        print('Ошибка')
+
+    space_counter = 0
+
+    while len(cabsec[c]) > 0:
+        if (len(line) + len(str(cabsec[c][0]))) < length:
+            if str(cabsec[c][0])[-1] == '0':
+                line = line + 'А' + str(cabsec[c][0]) + ', '
+                cabsec[c].pop(0)
+            else:
+                line = line + 'А' + str(cabsec[c][0]) + ', '
+                cabsec[c].pop(0)
+        else:
+            xl_app.Cells(counter, column + 1).Value = line[:-1]
+            line = ''
+            counter += 1
+            space_counter += 1
+
+    xl_app.Cells(counter, column + 1).Value = line[:-2]
+    line = ''
+    counter += 1
+    i += 1
+    if space_counter == 0:
+        xl_app.Cells(counter, column + 1).Value = ''
+        counter += 1
+
+end_line = 2000
+
+# Fill cable journal
+for i in range(1, end_line):
+    if xl_app.Cells(i, 1).Value == '' or xl_app.Cells(i, 1).Value == xl_app.Cells(i, 18).Value:
+        continue
+    else:
+        r = 'A' + str(i) + ':' + 'P' + str(i)
+        xl_app.Range(r).Insert()
+
+for i in range(1, end_line):
+    xl_app.Cells(i, 12).Value = xl_app.Cells(i, 19).Value
+
+xl_app.Columns(18).Clear()
+xl_app.Columns(19).Clear()
+
+
+# Add blank lines to fit document format (first page - 24 lines, subsequent pages - 30 lines)
+prev = 0
+page = 1
+lines1 = 24
+
+for current in range(1, lines1 + 10):
+    if xl_app.Cells(current, 1).Value != '' and xl_app.Cells(current, 1).Value is not None:
+        if prev <= lines1*page and current > lines1*page + 1:
+            j = prev
+            step = lines1*page - prev + 1
+            for j in range(step):
+                r = 'A' + str(prev) + ':' + 'P' + str(prev)
+                xl_app.Range(r).Insert()
+            page += 1
+        elif current > lines1*page:
+            page += 1
+        prev = current
+
+
+lines2 = 30
+page = 1
+
+for current in range(25, end_line):
+    if xl_app.Cells(current, 1).Value != '' and xl_app.Cells(current, 1).Value is not None:
+        if prev <= lines2*page + lines1 and current > lines2*page + lines1 + 1:
+            j = prev
+            step = lines2*page + lines1 - prev + 1
+            for j in range(step):
+                r = 'A' + str(prev) + ':' + 'P' + str(prev)
+                xl_app.Range(r).Insert()
+            page += 1
+        elif current > lines2*page + lines1:
+            page += 1
+        prev = current
+
+
+stop = time.time()
+print(round(stop - start), 'sec')
